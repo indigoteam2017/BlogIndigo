cd64706823b51a48bab00ccb200df67cd712bb3e
FedosovMaksym <maksymfedosov@gmail.com>
2017-11-06 02:29:15 +0200

    Added methods and classes.

--- src/main/java/com/indigoteam/controller/BlogController.java
+++ src/main/java/com/indigoteam/controller/BlogController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -25,4 +27,40 @@ public class BlogController {
         model.addAttribute("listBlogs",this.blogService.listBlogs());
         return "blogs";
     }
+
+    @RequestMapping(value = "/blogs/add", method = RequestMethod.POST)
+    public String addBlog(@ModelAttribute("blog") Blog blog){
+        if(blog.getId() == 0){
+            this.blogService.addBlog(blog);
+        }else{
+            this.blogService.updateBlog(blog);
+        }
+
+        return "redirect:/blogs";
+    }
+
+
+    @RequestMapping("/remove/{id}")
+    public String removeBlog(@PathVariable("id") int id){
+        this.blogService.removeBlog(id);
+
+        return "redirect:/blogs";
+    }
+
+    @RequestMapping("edit/{id}")
+    public String editBook(@PathVariable("id") int id, Model model){
+    model.addAttribute("blog", this.blogService.getBlogById(id));
+    model.addAttribute("listBooks", this.blogService.listBlogs());
+
+    return "redirect:/blogs";
+    }
+
+
+    @RequestMapping("blogdata/{id}")
+    public String blogData(@PathVariable("id")int id, Model model){
+        model.addAttribute("blog", this.blogService.getBlogById(id));
+
+        return "bookdata";
+    }
+
 }
